fd7ab47b9470f9181911120a8b772f61697d9ea8
Harshal Meshram <harshalmeshram112@gmail.com>
2025-02-19 13:13:52 +0530

    header added into env

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
-import "./Header.css"
+import React from "react";
+import "./Header.css";
 
 export default function Header() {
+  // Accessing the header title from the environment variables
+  const headerTitle = process.env.REACT_APP_HEADER_TITLE;
+
   return (
-    <header className="header"> 
-      <h2 className="header--title">MemeGenerator</h2>
+    <header className="header">
+      <h2 className="header--title">{headerTitle}</h2>
     </header>
   );
 }
